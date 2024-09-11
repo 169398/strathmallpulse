075fd2e4819baee91db6a5617232fb1896668982
idris <kulubiidris@gmail.com>
2024-09-11 16:18:15 +0300

    Update dependencies and add utility function for merging CSS classes,and metadata

---
+++ src/lib/metadata.ts
@@ -0,0 +1,57 @@
+import { Metadata } from "next";
+import { ensureStartsWith } from "./utils";
+
+const { TWITTER_CREATOR, TWITTER_SITE } = process.env;
+const twitterCreator = TWITTER_CREATOR
+  ? ensureStartsWith(TWITTER_CREATOR, "@")
+  : undefined;
+const twitterSite = TWITTER_SITE
+  ? ensureStartsWith(TWITTER_SITE, "https://")
+  : undefined;
+
+export function constructMetadata({
+  title = "StrathMallBuzz - Your Campus Social Hub",
+  description = "StrathMallBuzz connects university students with campus life through events, study groups, forums, and more. Explore resources, collaborate, and engage with your campus community.",
+  image = "/buzz-thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    ...(twitterCreator &&
+      twitterSite && {
+        twitter: {
+          card: "summary_large_image",
+          creator: twitterCreator,
+          site: twitterSite,
+          title,
+          description,
+          images: [image],
+        },
+      }),
+    icons,
+    metadataBase: new URL("https://strathmall.com/pulse"), 
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: true,
+      },
+    }),
+  };
+}
