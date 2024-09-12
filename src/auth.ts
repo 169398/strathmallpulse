7c8ba73fbffb24497e33ecba635e8f2abb9e56b6
idris <kulubiidris@gmail.com>
2024-09-12 14:41:18 +0300

    Refactor sellerindex.ts and .eslintrc.json

--- src/auth.ts
+++ src/auth.ts
@@ -1,27 +1,24 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { compareSync } from "bcrypt-ts-edge";
+import { eq } from "drizzle-orm";
+import type { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
 
-
-import { DrizzleAdapter } from '@auth/drizzle-adapter'
-import { compareSync } from 'bcrypt-ts-edge'
-import { eq } from 'drizzle-orm'
-import type { NextAuthConfig } from 'next-auth'
-import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import Google from 'next-auth/providers/google'
-import Resend from 'next-auth/providers/resend'
-
-import db from './db/drizzle'
-import {  users } from './db/schema'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-import { APP_NAME, SENDER_EMAIL } from './lib/constants'
+import db from "./db/drizzle";
+import {  users } from "./db/schema";
+import { NextResponse } from "next/server";
+import { APP_NAME, SENDER_EMAIL } from "./lib/constants";
 
 export const config = {
   pages: {
-    signIn: '/sign-in',
-    error: '/sign-in',
+    signIn: "/sign-in",
+    error: "/sign-in",
   },
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
   },
   adapter: DrizzleAdapter(db),
@@ -29,84 +26,92 @@ export const config = {
     CredentialsProvider({
       credentials: {
         email: {
-          type: 'email',
+          type: "email",
         },
-        password: { type: 'password' },
+        password: { type: "password" },
       },
       async authorize(credentials) {
-        if (credentials == null) return null
+        if (credentials == null) return null;
 
         const user = await db.query.users.findFirst({
           where: eq(users.email, credentials.email as string),
-        })
+        });
         if (user && user.password) {
           const isMatch = compareSync(
             credentials.password as string,
             user.password
-          )
+          );
           if (isMatch) {
             return {
               id: user.id,
               name: user.name,
               email: user.email,
               role: user.role,
-            }
+            };
           }
         }
-        return null
+        return null;
       },
     }),
     Resend({
-      name: 'Email',
+      name: "Email",
       from: `${APP_NAME} <${SENDER_EMAIL}>`,
-      id: 'email',
+      id: "email",
     }),
     Google({
       allowDangerousEmailAccountLinking: true,
+      clientId: "",
+      clientSecret: "",
     }),
   ],
   callbacks: {
     jwt: async ({ token, user, trigger, session }: any) => {
       if (user) {
-        if (user.name === 'NO_NAME') {
-          token.name = user.email!.split('@')[0]
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
           await db
             .update(users)
             .set({
               name: token.name,
             })
-            .where(eq(users.id, user.id))
+            .where(eq(users.id, user.id));
         }
 
-        token.role = user.role
-       
+        token.role = user.role;
       }
 
-      if (session?.user.name && trigger === 'update') {
-        token.name = session.user.name
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
       }
 
-      return token
+      return token;
     },
     session: async ({ session, user, trigger, token }: any) => {
-      session.user.id = token.sub
-      session.user.role = token.role
-      if (trigger === 'update') {
-        session.user.name = user.name
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      if (trigger === "update") {
+        session.user.name = user.name;
       }
-      return session
+      return session;
     },
     authorized({ request, auth }: any) {
-      const protectedPaths = [
-        
-        /\/profile/,
-        /\/user\/(.*)/,
-        /\/admin/,
-      ]
-      const { pathname } = request.nextUrl
-      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false
-     
+      const protectedPaths = [/\/profile/, /\/user\/(.*)/, /\/admin/];
+      const { pathname } = request.nextUrl;
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+      if (!request.cookies.get("sessionCartId")) {
+        const sessionCartId = crypto.randomUUID();
+        const newRequestHeaders = new Headers(request.headers);
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+        response.cookies.set("sessionCartId", sessionCartId);
+        return response;
+      } else {
+        return true;
+      }
     },
   },
-} satisfies NextAuthConfig
-export const { handlers, auth, signIn, signOut } = NextAuth(config)
+} satisfies NextAuthConfig;
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,39 +1,36 @@
-'use server'
+"use server";
 
-import { isRedirectError } from 'next/dist/client/components/redirect'
+import { isRedirectError } from "next/dist/client/components/redirect";
+
+import { auth, signIn, signOut } from "@/auth";
 import {
- 
+  
   signInFormSchema,
   signUpFormSchema,
   updateUserSchema,
-} from '../validator'
-import { formatError } from '../utils'
-import { hashSync } from 'bcrypt-ts-edge'
-import {  users } from '@/db/schema'
-import { count, desc, eq } from 'drizzle-orm'
-import { revalidatePath } from 'next/cache'
-import { z } from 'zod'
-import { PAGE_SIZE } from '../constants'
+} from "../validator";
+import { formatError } from "../utils";
+import { hashSync } from "bcrypt-ts-edge";
+import {  users } from "@/db/schema";
+import { count, desc, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { PAGE_SIZE } from "../constants";
 import db from "@/db/drizzle";
 
-import { sendVerificationEmail } from '@/emailverify'
-import { sendResetPasswordEmail } from '@/emailreset-password'
-import { addMinutes } from 'date-fns'
-import { auth, signIn, signOut } from '@/auth'
-
-
+import { sendVerificationEmail } from "@/emailverify";
+import { sendResetPasswordEmail } from "@/emailreset-password";
+import { addMinutes } from "date-fns";
 
 // USER
 
 export async function signUp(prevState: unknown, formData: FormData) {
-
-try {
-
-  const user = signUpFormSchema.parse({
-      name: formData.get('name'),
-      email: formData.get('email'),
-      confirmPassword: formData.get('confirmPassword'),
-      password: formData.get('password'),
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      confirmPassword: formData.get("confirmPassword"),
+      password: formData.get("password"),
     });
     const values = {
       id: crypto.randomUUID(),
@@ -43,7 +40,6 @@ try {
 
     await db.insert(users).values(values);
 
- 
     await sendVerificationEmail({
       name: "",
       resetToken: "",
@@ -54,24 +50,24 @@ try {
       role: "",
       emailVerified: null,
       image: null,
-      address: null,
-      paymentMethod: null,
       createdAt: null,
     });
 
-    return { success: true, message: 'User created successfully. Please check your email for verification.' };
-  
-} catch (error) {
-   return {
+    return {
+      success: true,
+      message:
+        "User created successfully. Please check your email for verification.",
+    };
+  } catch (error) {
+    return {
       success: false,
       message: formatError(error).includes(
-        'duplicate key value violates unique constraint "user_email_idx"',
+        'duplicate key value violates unique constraint "user_email_idx"'
       )
-        ? 'Email already exists'
+        ? "Email already exists"
         : formatError(error),
-}
-
-}
+    };
+  }
 }
 export async function signInWithCredentials(
   prevState: unknown,
@@ -79,30 +75,30 @@ export async function signInWithCredentials(
 ) {
   try {
     const user = signInFormSchema.parse({
-      email: formData.get('email'),
-      password: formData.get('password'),
-    })
-    await signIn('credentials', user)
-    return { success: true, message: 'Sign in successfully' }
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+    await signIn("credentials", user);
+    return { success: true, message: "Sign in successfully" };
   } catch (error) {
     if (isRedirectError(error)) {
-      throw error
+      throw error;
     }
-    return { success: false, message: 'Invalid email or password' }
+    return { success: false, message: "Invalid email or password" };
   }
 }
 
-export const SignInWithEmail = async (formData: any) => {
-  await signIn('email', formData)
-}
+export const SignInWithEmail = async (formData:any) => {
+  await signIn("email", formData);
+};
 
 export const SignInWithGoogle = async () => {
-  await signIn('google')
-}
+  await signIn("google");
+};
 
 export const SignOut = async () => {
-  await signOut()
-}
+  await signOut();
+};
 
 //RESET PASSWORD
 const requestResetSchema = z.object({
@@ -118,7 +114,7 @@ const resetPasswordSchema = z.object({
 export const requestPasswordReset = async (formData: FormData) => {
   try {
     const data = requestResetSchema.parse({
-      email: formData.get('email'),
+      email: formData.get("email"),
     });
 
     const user = await db.query.users.findFirst({
@@ -126,23 +122,25 @@ export const requestPasswordReset = async (formData: FormData) => {
     });
 
     if (!user) {
-      throw new Error('No user found with this email address.');
+      throw new Error("No user found with this email address.");
     }
 
     // Generate a reset token
     const resetToken = crypto.randomUUID();
-    const expiresAt = addMinutes(new Date(), 60); 
-console.log(resetToken)
+    const expiresAt = addMinutes(new Date(), 60);
+    console.log(resetToken);
     await db
       .update(users)
-      .set({ resetToken, resetTokenExpires: expiresAt }) 
+      .set({ resetToken, resetTokenExpires: expiresAt })
       .where(eq(users.id, user.id));
 
     // Send reset email
     await sendResetPasswordEmail(user, resetToken);
 
-
-    return { success: true, message: 'Password reset email sent successfully.' };
+    return {
+      success: true,
+      message: "Password reset email sent successfully.",
+    };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
@@ -150,83 +148,87 @@ console.log(resetToken)
 
 export const resetPassword = async (formData: FormData) => {
   try {
-    const token = formData.get('token') as string;
-    const newPassword = formData.get('newPassword') as string;
+    const token = formData.get("token") as string;
+    const newPassword = formData.get("newPassword") as string;
 
     const data = resetPasswordSchema.parse({
       token,
       newPassword,
     });
 
-
     const user = await db.query.users.findFirst({
       where: (users, { eq }) => eq(users.resetToken, data.token),
     });
 
-
     if (!user) {
-      throw new Error('Invalid or expired reset token.');
+      throw new Error("Invalid or expired reset token.");
     }
 
-    if (user.resetTokenExpires && new Date() > new Date(user.resetTokenExpires)) {
-      throw new Error('Reset token has expired.');
+    if (
+      user.resetTokenExpires &&
+      new Date() > new Date(user.resetTokenExpires)
+    ) {
+      throw new Error("Reset token has expired.");
     }
 
     const hashedPassword = hashSync(data.newPassword, 10);
     await db
       .update(users)
-      .set({ password: hashedPassword, resetToken: null, resetTokenExpires: null })
+      .set({
+        password: hashedPassword,
+        resetToken: null,
+        resetTokenExpires: null,
+      })
       .where(eq(users.id, user.id));
 
-    revalidatePath('/sign-in');
+    revalidatePath("/sign-in");
 
-    return { success: true, message: 'Password reset successfully.' };
+    return { success: true, message: "Password reset successfully." };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
 };
-
 
 // GET
 export async function getAllUsers({
   limit = PAGE_SIZE,
   page,
 }: {
-  limit?: number
-  page: number
+  limit?: number;
+  page: number;
 }) {
   const data = await db.query.users.findMany({
     orderBy: [desc(users.createdAt)],
     limit,
     offset: (page - 1) * limit,
-  })
-  const dataCount = await db.select({ count: count() }).from(users)
+  });
+  const dataCount = await db.select({ count: count() }).from(users);
   return {
     data,
     totalPages: Math.ceil(dataCount[0].count / limit),
-  }
+  };
 }
 
 export async function getUserById(userId: string) {
   const user = await db.query.users.findFirst({
     where: (users, { eq }) => eq(users.id, userId),
-  })
-  if (!user) throw new Error('User not found')
-  return user
+  });
+  if (!user) throw new Error("User not found");
+  return user;
 }
 
 // DELETE
 
 export async function deleteUser(id: string) {
   try {
-    await db.delete(users).where(eq(users.id, id))
-    revalidatePath('/admin/users/seller')
+    await db.delete(users).where(eq(users.id, id));
+    revalidatePath("/admin/users/seller");
     return {
       success: true,
-      message: 'User deleted successfully',
-    }
+      message: "User deleted successfully",
+    };
   } catch (error) {
-    return { success: false, message: formatError(error) }
+    return { success: false, message: formatError(error) };
   }
 }
 
@@ -237,41 +239,38 @@ export async function updateUser(user: z.infer<typeof updateUserSchema>) {
       .update(users)
       .set({
         name: user.name,
+        role: user.role,
       })
-      .where(eq(users.id, user.id))
-    revalidatePath('/admin/users')
+      .where(eq(users.id, user.id));
+    revalidatePath("/admin/users");
     return {
       success: true,
-      message: 'User updated successfully',
-    }
+      message: "User updated successfully",
+    };
   } catch (error) {
-    return { success: false, message: formatError(error) }
+    return { success: false, message: formatError(error) };
   }
 }
 
-
 export async function updateProfile(user: { name: string; email: string }) {
   try {
-    const session = await auth()
+    const session = await auth();
     const currentUser = await db.query.users.findFirst({
       where: (users, { eq }) => eq(users.id, session?.user.id!),
-    })
-    if (!currentUser) throw new Error('User not found')
+    });
+    if (!currentUser) throw new Error("User not found");
     await db
       .update(users)
       .set({
         name: user.name,
       })
-      .where(eq(users.id, currentUser.id))
+      .where(eq(users.id, currentUser.id));
 
     return {
       success: true,
-      message: 'User updated successfully',
-    }
+      message: "User updated successfully",
+    };
   } catch (error) {
-    return { success: false, message: formatError(error) }
+    return { success: false, message: formatError(error) };
   }
 }
-
-
-
